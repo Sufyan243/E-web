050d546a1fd82f2502721b7d211df609b41d6c96
Sufyan <Syedmuhammadsufyan237@gmail.com>
2024-11-27 03:22:23 +0500

    first commit

---
+++ JS/SCript.js
@@ -0,0 +1,10 @@
+// Function to toggle visibility of comparison table
+function toggleComparison() {
+    let comparisonTable = document.getElementById("comparisonTable");
+    if (comparisonTable.style.display === "none") {
+      comparisonTable.style.display = "block";
+    } else {
+      comparisonTable.style.display = "none";
+    }
+  }
+  
